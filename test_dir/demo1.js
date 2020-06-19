2b4bf26d2fbb7dac81492b3a0467e175bcefb033
Allen <1480059245@qq.com>
2020-06-19 14:26:18 +0800

    分支合并

--- test_dir/demo1.js
+++ test_dir/demo1.js
@@ -2,12 +2,12 @@ function say(){
     console.log("this is a person");
 }
 
-function shifa(){
-    
-} 
-
 
 function master(){
     console.log("这是master分支")
 }
 
+
+function me(){
+    console.log("我要合并分支了");
+}
